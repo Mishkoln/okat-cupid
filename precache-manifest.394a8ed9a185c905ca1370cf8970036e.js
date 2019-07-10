cddd05f4b75228513ed4a03e90698c9ad08388b7
Mishkoln <shkolnik.mi@gmail.com>
2019-07-10 20:05:17 +0300

    Updates

---
+++ precache-manifest.394a8ed9a185c905ca1370cf8970036e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90561411c6d0843450a2ec3be57797fd",
+    "url": "/okat-cupid/index.html"
+  },
+  {
+    "revision": "286a81aef5adc24ec5d2",
+    "url": "/okat-cupid/static/css/main.ca82ad72.chunk.css"
+  },
+  {
+    "revision": "3730b3db477ca89fea59",
+    "url": "/okat-cupid/static/js/2.2f91856e.chunk.js"
+  },
+  {
+    "revision": "286a81aef5adc24ec5d2",
+    "url": "/okat-cupid/static/js/main.bc2925cf.chunk.js"
+  },
+  {
+    "revision": "ac4d2b7ca81d0f804757",
+    "url": "/okat-cupid/static/js/runtime~main.2982c164.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/okat-cupid/precache-manifest.132bbc19f53c7cc9e3f4d7fc12b7d291.js"
+  "/okat-cupid/precache-manifest.394a8ed9a185c905ca1370cf8970036e.js"
 );
 
 self.addEventListener('message', (event) => {
